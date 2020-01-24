284d3ce8e8ebeeddb47f065ffa0a5210d4c0ae90
Philipp <philipp.gatzka@gmx.ch>
2020-01-24 14:05:24 +0100

    do it

--- Scripts/Classes/Cinema.php
+++ Scripts/Classes/Cinema.php
@@ -3,5 +3,51 @@
 
 class Cinema
 {
+    private $cinema = [];
+    private $filename = '';
+    private $timePlan = [];
+
+    public function addRoom(): Room
+    {
+        return null;
+    }
+
+    public function setTimePlan(): void
+    {
+        return null;
+    }
+
+    /**
+     * @param string $filename
+     */
+    public function setFilename(string $filename): void
+    {
+        $this->filename = $filename;
+    }
+
+    public function saveData(): void
+    {
+        file_put_contents($this->filename,$this->encodeData());
+    }
+
+    public function loadData(): array
+    {
+        return $this->decodeData(file_get_contents($this->filename));
+    }
+
+    public function decodeData($json): array
+    {
+        return json_decode($json, JSON_OBJECT_AS_ARRAY);
+    }
+
+    public function encodeData(): string
+    {
+
+    }
+
+    public function createFromArray(): Cinema
+    {
+        return null;
+    }
 
 }
--- Scripts/Classes/Movie.php
+++ Scripts/Classes/Movie.php
@@ -3,5 +3,37 @@
 
 class Movie
 {
+    private $movieName = '';
+    private $displayTimes = [];
+    private $displayRooms = [];
+
+    public function createMovie(): Movie
+    {
+    }
+
+    public function addDisplayTime(): void
+    {
+    }
+
+    public function removeDisplay(): void
+    {
+    }
+
+    public function getDisplayTimes(): array
+    {
+    }
+
+    public function timesToString(): string
+    {
+    }
+
+    public function setMovieName(): void
+    {
+    }
+
+    public function toArray(): array
+    {
+    }
+
 
 }
