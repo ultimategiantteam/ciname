7201922019f264eee38300a11c20e2b1a7c41a3d
Philipp <philipp.gatzka@gmx.ch>
2020-01-29 09:43:17 +0100

    i ROBBED sina's virginity

--- Project/Scripts/Classes/Seat.php
+++ Project/Scripts/Classes/Seat.php
@@ -3,5 +3,16 @@
 
 class Seat
 {
+    private bool $reserved = false;
+    private int $seatID = 0;
 
+    public function changeReservation(): void
+    {
+        $this->reserved = !$this->reserved;
+    }
+
+    public function setSeatID(int $id): void
+    {
+        $this->seatID = $id;
+    }
 }
