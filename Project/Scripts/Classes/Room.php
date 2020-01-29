88028636c682fdf5a1384d577b58a9ef8d7a7326
Fauleinwurf <nino.nufrio@outlook.de>
2020-01-29 10:23:26 +0100

    movies added

--- Project/Scripts/Classes/Room.php
+++ Project/Scripts/Classes/Room.php
@@ -3,5 +3,19 @@
 
 class Room
 {
+    private string $name = '';
+    private int $cols;
+    private int $rows;
+    private array $seats = [];
 
+    public function createSeatMap(int $rows, int $cols): void
+    {
+        $this->rows = $rows;
+        $this->cols = $cols;
+        for ($x = 0; $x < $rows; $x++) {
+            for ($y = 0; $y < $cols; $y++) {
+                $this->seats .= ;
+            }
+        }
+    }
 }
--- Project/Scripts/Classes/Seat.php
+++ Project/Scripts/Classes/Seat.php
@@ -3,5 +3,11 @@
 
 class Seat
 {
+    private bool $reserved = false;
+    private int $seatID;
 
+    public static function createSeat():Seat
+    {
+
+    }
 }
