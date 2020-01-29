db48d39715288bfa177fcfbcd82a09bb32ecbd91
Fauleinwurf <nino.nufrio@outlook.de>
2020-01-29 10:32:00 +0100

    push

--- Project/Scripts/Classes/Seat.php
+++ Project/Scripts/Classes/Seat.php
@@ -6,7 +6,14 @@ class Seat
     private bool $reserved = false;
     private int $seatID;
 
-    public static function createSeat():Seat
+    public static function createSeat(): Seat
+    {
+        return new Seat();
+    }
+
+
+
+    public function setSeatID():void
     {
 
     }
