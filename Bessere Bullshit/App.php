ee7c03363f9e4fcb12976b0afc1fe6ddd62099a2
Philipp <philipp.gatzka@gmx.ch>
2020-01-31 08:13:57 +0100

    Redo Everything

--- Bessere Bullshit/App.php
+++ Bessere Bullshit/App.php
@@ -17,22 +17,17 @@ class App
         $filename = './cinema.json';
 
         $cinema = Cinema::createFromFile($filename);
-        /*
         $cinema->addRoom(Room::createFromConsole());
         $cinema->addMovie(Movie::createFromConsole());
-        */
         $cinema->persist($filename);
         $cinema->addShow(Show::createFromConsole($filename));
-
         foreach ($cinema->getRooms() as $room) {
             printf('%s [%s]' . PHP_EOL, $room->getName(), $room->getId());
         }
         foreach ($cinema->getShows() as $show) {
             printf('%s %s %s [%s]' . PHP_EOL, $show->getMovie(), $show->getRoom(), $show->getTime(), $show->getId());
         }
-
         $cinema->getRoom(0)->getName();
-
         $cinema->persist($filename);
     }
 }
